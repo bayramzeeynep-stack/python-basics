0201b28382a1acaa9e7bd80b2cafb439460bfda9
Zeynep <bayramzeeynep@gmail.com>
2026-01-15 19:31:26 +0300

    Add function to calculate rectangle area

---
+++ basics/rectangle_area.py
@@ -0,0 +1,8 @@
+def dikdörtgenalanbulma(kenar1,kenar2):
+    return kenar1*kenar2
+   
+kenar1=int(input("Kenar1 için sayı giriniz"))
+kenar2=int(input("Kenar2 için sayi giriniz"))
+
+alan=dikdörtgenalanbulma(kenar1,kenar2)
+print("Dikdörtgen alanı:", alan) 
